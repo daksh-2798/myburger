de2092a0a03fb13425ee9364fba8aab9a7d49c17
daky-2798 <dakshtandel.27@gmail.com>
2021-06-24 15:52:05 +0530

    Custom input component

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../components/UI/Button/Button';
 import styles from '../ContactData/ContactData.module.css';
 import axios from '../../axio-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import Input from '../../components/UI/Input/Input';
 
 class ContactData extends Component{
 
@@ -49,10 +50,10 @@ class ContactData extends Component{
     render(){
         let form = (
                 <form>
-                    <input type="text" name="name" placeholder="Your Name"/>
-                    <input type="email" name="email" placeholder="Your Email id"/>
-                    <input type="text" name="name" placeholder="Your Street Name"/>
-                    <input type="text" name="name" placeholder="Your Postal Code"/><br/>
+                    <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
+                    <Input inputtype="input" type="email" name="email" placeholder="Your Email id"/>
+                    <Input inputtype="input" type="text" name="name" placeholder="Your Street Name"/>
+                    <Input inputtype="input" type="text" name="name" placeholder="Your Postal Code"/><br/>
                     <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
                 </form>
         );
