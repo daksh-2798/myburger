418412f276f62371ef033e542ee5c35b7ac4b135
daky-2798 <dakshtandel.27@gmail.com>
2021-06-25 20:21:30 +0530

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import reducer from './store/reducer';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 const app = (
+  <Provider store={store}>
   <BrowserRouter>
     <App/>
   </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app,document.getElementById('root'));
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actions';
+
+const initialState ={
+    ingredient: null,
+    totalPrice : 45
+};
+
+const reducer = (state = initialState,action) => {
+
+};
+
+export default reducer;
