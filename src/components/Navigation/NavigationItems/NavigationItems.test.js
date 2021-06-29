aa56de5c038307b6052daaae6d88d3cc523eadaa
daky-2798 <dakshtandel.27@gmail.com>
2021-06-29 16:22:43 +0530

    Tested NavigationItems component using Jest,Enzyme

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {configure , shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+
+configure({adapter : new Adapter()});
+
+describe('<NavigationItems/>', ()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(<NavigationItems />);
+    });
+    it('should render two <NavigationItem/> elements if not authenticated' , ()=>{
+    //const wrapper = shallow(<NavigationItems />);
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem/> elements if authenticated' , ()=>{
+        //const wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated : true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+    it('should be an exact logout button if authenticated' , ()=>{
+        //const wrapper = shallow(<NavigationItems isAuthenticated />);
+            wrapper.setProps({isAuthenticated : true});
+            expect(wrapper.contains(<NavigationItem link="/logout">LogOut</NavigationItem>)).toEqual(true);
+    });
+})
